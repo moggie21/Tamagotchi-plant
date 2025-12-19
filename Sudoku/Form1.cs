a8943f98fa884dc265620be5eace7aaf7f04f20c
yana kiseleva <kvak2065@gmail,com>
2025-12-19 23:19:02 +1000

    базовый интерфейс

--- Sudoku/Form1.cs
+++ Sudoku/Form1.cs
@@ -6,5 +6,28 @@ namespace Sudoku
         {
             InitializeComponent();
         }
+
+        public void ShowNewGamePanel()
+        {
+            mainPanel.Visible = false;
+            newGamePanel.Visible = true;
+        }
+
+        public void ShowMainPanel()
+        {
+            newGamePanel.Visible = false;
+            mainPanel.Visible = true;
+            
+        }
+
+        private void newGameBtn_Click(object sender, EventArgs e)
+        {
+            ShowNewGamePanel();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            ShowMainPanel();
+        }
     }
 }
